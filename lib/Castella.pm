d186ec701c0a0666055a60c50ade5a2f480bc9c9
Nishibayashi Takuji <takuji@senchan.jp>
2011-04-20 07:31:18 +0900

    initial commit

---
+++ lib/Castella.pm
@@ -0,0 +1,33 @@
+package Castella;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Castella -
+
+=head1 SYNOPSIS
+
+  use Castella;
+
+=head1 DESCRIPTION
+
+Castella is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
